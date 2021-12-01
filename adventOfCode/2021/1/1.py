8cc6e897f989db5e846e4c83de5b6f1ae91234f5
Jiří Thran Řihák <exthran@gmail.com>
2021-12-01 10:20:44 +0100

    AoC 1

---
+++ adventOfCode/2021/1/1.py
@@ -0,0 +1,34 @@
+# start: 9:33
+# 1.:    9:37
+# 2.:    9:41
+
+from core import AdventOfCode
+
+
+class Level(AdventOfCode):
+
+    @property
+    def part_one_test_solution(self):
+        return 7
+
+    @property
+    def part_two_test_solution(self):
+        return 5
+
+    def get_increases(self, values):
+        increases = 0
+        last_value = values[0]
+        for depth in values[1:]:
+            if last_value < depth:
+                increases += 1
+            last_value = depth
+        return increases
+
+    def part_one(self, depths) -> int:
+        return self.get_increases(depths)
+
+    def part_two(self, depths) -> int:
+        return self.get_increases([sum(depths[i:i + 3]) for i in range(len(depths) - 2)])
+
+
+Level().run()
