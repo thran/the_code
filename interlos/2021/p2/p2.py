a752c0d249cf7088302682fffc6a76c49cb5fc44
Jiří Thran Řihák <exthran@gmail.com>
2021-12-05 12:55:11 +0100

    solve interlos P2

--- interlos/2021/p2/p2.py
+++ interlos/2021/p2/p2.py
@@ -1,9 +1,5 @@
-import numpy as np
 import pandas as pd
-from tqdm import tqdm
-from icecream import ic
-
-from utils import memoize
+from munkres import Munkres  # assigment problem
 
 losi = pd.read_csv('losi-priklad.csv', index_col=0)
 losice = pd.read_csv('losice-priklad.csv', index_col=0)
@@ -12,35 +8,12 @@ losice = pd.read_csv('losice.csv', index_col=0)
 
 
 s = losi + losice
-s[losi < 4 ] = None
-s[losice < 4 ] = None
-s = s.values
+s[losi < 4 ] = -10000
+s[losice < 4 ] = -10000
+m = 20 - s.values
 
-print(s.shape)
+total = 0
+for match in Munkres().compute(m):
+    total += s.values[match]
 
-top = 0
-
-
-@memoize
-def possible_combinations(ns, m=0):
-    global top
-    if len(ns) == 1:
-        v = s[-1, ns[0]]
-        if pd.notna(v) and v + m > top:
-            top = v + m
-            print(top)
-        return v
-
-    results = []
-    for i, n in sorted(enumerate(s[-len(ns), ns]), key=lambda x: -x[1]):
-        if pd.notna(n):
-            r = possible_combinations(ns[:i] + ns[i + 1:], m + n)
-            if pd.notna(r):
-                results.append(n + r)
-    if results:
-        return max(results)
-    return np.nan
-
-
-# ic(possible_combinations((0, )))
-print(possible_combinations(tuple(range(len(s)))))
+print(total)
