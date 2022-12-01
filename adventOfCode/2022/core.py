af1cc64c646e565badf5ca3527ecd2c920335fda
Jiří Thran Řihák <exthran@gmail.com>
2022-12-01 09:39:37 +0100

    AoC prepare for new year

---
+++ adventOfCode/2022/core.py
@@ -0,0 +1,49 @@
+from pathlib import Path
+from icecream import install
+
+install()
+
+
+class AdventOfCode:
+    part_one_test_solution = None
+    part_two_test_solution = None
+
+    @property
+    def test_input(self):
+        return self.load_file('input.test.txt')
+
+    @property
+    def input(self):
+        return self.load_file('input.txt')
+
+    def load_file(self, file_path):
+        with Path(file_path).open() as f:
+            lines = list(map(lambda l: l.strip(), f.readlines()))
+            input_ = self.preprocess_input(lines)
+            if type(input_) is not tuple:
+                return input_,
+            return input_
+
+    def preprocess_input(self, lines):
+        if all(line.isnumeric() for line in lines):
+            lines = list(map(int, lines))
+        if len(lines) == 0:
+            return lines[0]
+
+        return list(lines)
+
+    def part_one(self, *input_) -> int:
+        pass
+
+    def part_two(self, *input_) -> int:
+        pass
+
+    def run(self):
+        part_one = self.part_one(*self.test_input)
+        assert part_one == self.part_one_test_solution, f'invalid solution {part_one}'
+        print(f'part 1: {self.part_one(*self.input)}')
+
+        if self.part_two_test_solution is not None:
+            part_two = self.part_two(*self.test_input)
+            assert  part_two == self.part_two_test_solution, f'invalid solution {part_two}'
+            print(f'part 2: {self.part_two(*self.input)}')
---
+++ adventOfCode/2022/template/n.py
@@ -0,0 +1,15 @@
+from core import AdventOfCode
+
+
+class Level(AdventOfCode):
+    part_one_test_solution = None
+    part_two_test_solution = None
+
+    def part_one(self, lines) -> int:
+        ...
+
+    def part_two(self, lines) -> int:
+        ...
+
+
+Level().run()
