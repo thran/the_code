bd3e1f323526a48466d57c75e1d7b3539db5a3d3
Thran <thran@centrum.cz>
2022-12-20 00:01:13 +0100

    AoC 19 - hell on earth!

--- adventOfCode/2022/19/19.py
+++ adventOfCode/2022/19/19.py
@@ -1,4 +1,3 @@
-import math
 from collections import defaultdict
 from functools import cache
 
@@ -29,86 +28,74 @@ class Level(AdventOfCode):
 
         return blueprints
 
-    @staticmethod
-    def can_build(blueprint, supplies, robot: str):
-        return all(s >= cost for s, cost in zip(supplies, blueprint[robot]))
+    def can_build(self, supplies, robot: str):
+        return all(s >= cost for s, cost in zip(supplies, self.blueprint[robot]))
 
-    def get_robot_creation_options(self, blueprint, supplies, robots, remaining_steps):
+    def compute_supplies(self, supplies, robots, new_robot):
+        if new_robot:
+            robots_cost = self.blueprint[new_robot] + [0]
+        else:
+            robots_cost = (0, 0, 0, 0)
+        return tuple(r + s - rc for r, s, rc in zip(robots, supplies, robots_cost))
+
+    def get_robot_creation_options(self, supplies, robots, remaining_steps):
         options = []
 
-        if self.can_build(blueprint, supplies, 'geode'):
-            return [(0, 0, 0, 1)]
-        if remaining_steps < 2:
-            return
-
-        if self.can_build(blueprint, supplies, 'obsidian'):
-            options.append((0, 0, 1, 0))
-            # if supplies[1] + 2 * robots[1] < 2 * blueprint['obsidian'][1] <= supplies[1] + 2 * robots[1] + 3:
-            #     ic(supplies, robots, remaining_steps)
-            # else:
-            #     return options
+        if self.can_build(supplies, 'geode'):
+            options.append('geode')
+            return options
 
         if remaining_steps < 3:
-            return
-        if self.can_build(blueprint, supplies, 'clay'):
-            if supplies[1] < blueprint['obsidian'][1] * 2:
-                options.append((0, 1, 0, 0))
+            return options
 
-        if remaining_steps < 4:
-            return
-        if robots[0] < max(c[0] for r, c in blueprint.items() if r != 'ore'):
-            if self.can_build(blueprint, supplies, 'ore'):
-                options.append((1, 0, 0, 0))
-            options.append((0, 0, 0, 0))
+        if self.can_build(supplies, 'obsidian'):
+            options.append('obsidian')
+
+        if self.can_build(supplies, 'clay'):
+            options.append('clay')
+
+        if robots[0] < self.max_ore_price:
+            if self.can_build(supplies, 'ore'):
+                options.append('ore')
+            options.append(None)
 
         return options
 
     @cache
-    def maximize_geodes(self, blueprint_index, robots, supplies, remaining_steps):
-        # ic(robots, supplies, remaining_steps)
+    def maximize_geodes(self, robots, supplies, remaining_steps):
         if remaining_steps == 0:
             return supplies[-1]
 
         if remaining_steps == 1:
             return supplies[-1] + robots[-1]
 
-        if (
-            supplies[-1] == 0
-            and supplies[-2] == 0
-            and remaining_steps < self.blueprints[blueprint_index]['geode'][2] / 2
-        ):
-
-            return 0
-
         results = []
-        for new_robots in self.get_robot_creation_options(
-            self.blueprints[blueprint_index], supplies, robots, remaining_steps
-        ) or [(0, 0, 0, 0)]:
-            robots_cost = [0, 0, 0, 0]
-            for material, robot_count in zip(self.MATERIALS, new_robots):
-                for i, material_cost in enumerate(self.blueprints[blueprint_index][material]):
-                    robots_cost[i] += material_cost * robot_count
-            new_supplies = tuple(r + s - rc for r, s, rc in zip(robots, supplies, robots_cost))
-            new_robots = tuple(r + nr for r, nr in zip(robots, new_robots))
-            r = self.maximize_geodes(blueprint_index, new_robots, new_supplies, remaining_steps - 1)
+        for new_robot in self.get_robot_creation_options(supplies, robots, remaining_steps) or [None]:
+            new_supplies = self.compute_supplies(supplies, robots, new_robot)
+            new_robots = tuple(r + 1 if self.MATERIALS[i] == new_robot else r for i, r in enumerate(robots))
+            r = self.maximize_geodes(new_robots, new_supplies, remaining_steps - 1)
             results.append(r)
         return max(results)
 
+    def set_blueprint(self, blueprint):
+        self.blueprint = blueprint
+        self.max_ore_price = max(c[0] for r, c in blueprint.items() if r != 'ore')
+        self.max_clay_price = max(c[1] for r, c in blueprint.items())
+        self.maximize_geodes.cache_clear()
+
     def part_one(self, blueprints) -> int:
         quality = 0
-        for i in blueprints:
-            self.maximize_geodes.cache_clear()
-            geodes = self.maximize_geodes(i, (1, 0, 0, 0), (0, 0, 0, 0), 24)
-            ic(i, geodes)
+        for i, blueprint in blueprints.items():
+            self.set_blueprint(blueprint)
+            geodes = self.maximize_geodes((1, 0, 0, 0), (0, 0, 0, 0), 24)
             quality += i * geodes
         return quality
 
     def part_two(self, blueprints) -> int:
         quality = 1
         for i in range(1, min(3, len(blueprints)) + 1):
-            self.maximize_geodes.cache_clear()
-            geodes = self.maximize_geodes(i, (1, 0, 0, 0), (0, 0, 0, 0), 32)
-            ic(i, geodes)
+            self.set_blueprint(blueprints[i])
+            geodes = self.maximize_geodes((1, 0, 0, 0), (0, 0, 0, 0), 32)
             quality *= geodes
         return quality
 
