9a2f1d9841fd150ca2f2247d6037ff5b6ec0e215
Jiří Thran Řihák <exthran@gmail.com>
2024-12-01 08:58:27 +0100

    AoC init 2024

---
+++ adventOfCode/2024/template/n.py
@@ -0,0 +1,16 @@
+from core import AdventOfCode
+
+
+class Level(AdventOfCode):
+    part_one_test_solution = None
+    part_two_test_solution = None
+
+    def part_one(self, lines) -> int:
+        ...
+
+    def part_two(self, lines) -> int:
+        ...
+
+
+if __name__ == '__main__':
+    Level().run()
